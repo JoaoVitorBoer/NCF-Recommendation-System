3a57472193dda70d7ff3ca15db13af5c08954aec
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2024-06-04 20:39:58 -0300

    Recommending methods

--- recommend.py
+++ recommend.py
@@ -2,9 +2,6 @@ from models.NeuMF import NeuMF
 from utils.read_files import read_yaml_file, read_jokes_csv_file, read_ratings_csv_file
 import numpy as np
 
-RATINGS_PATH = './data/preprocessed_data/ratings/ratings_preprocessed_ml.csv'
-JOKES_PATH = './data/preprocessed_data/jokes/jokes_preprocessed.csv'
-
 def recommend_items_for_existing_user(user_id, num_recommendations=10):
     data = read_yaml_file()
     ratings = read_ratings_csv_file()
@@ -19,29 +16,24 @@ def recommend_items_for_existing_user(user_id, num_recommendations=10):
 
     neumf_model.load_weights('./data/weights/neumf.h5')
 
-    user_id = np.full(num_items, user_id)
-    jokes_ids = np.arange(1, num_items+1)
-    pred = neumf_model.predict([user_id, jokes_ids])
+    user_id_list = np.full(num_items, user_id) # user_id is repeated for all jokes
+    jokes_ids = np.arange(1, num_items+1) # joke_id is from 1 to num_items
+    pred = neumf_model.predict([user_id_list, jokes_ids]) 
     top_k = np.argsort(pred, axis=0).reshape(-1)[::-1][:num_recommendations]
-    top_k_jokes = jokes.iloc[top_k]
+    top_k_jokes = jokes.iloc[top_k] # get the top k jokes text
     return top_k_jokes.values
 
 def recommend_items_for_new_user(num_recommendations=10):
     ratings = read_ratings_csv_file()
     jokes = read_jokes_csv_file()
     
-    most_popular_jokes_idx = ratings.value_counts('joke_id').sort_values(ascending=False)
+    most_popular_jokes_idx = ratings.value_counts('joke_id').sort_values(ascending=False) # get the most popular jokes
     top_k_popular_jokes = most_popular_jokes_idx.index[:num_recommendations]
     pop_jokes = jokes.iloc[top_k_popular_jokes]
     return pop_jokes.values
 
 
-
-
-
-
-
-def main():
+def recommend_new_item_for_existing_user(user_id):
     data = read_yaml_file()
     ratings = read_ratings_csv_file()
     jokes = read_jokes_csv_file()
@@ -53,11 +45,24 @@ def main():
     neumf_model = neumf.build_model()
     neumf_model = neumf.compile_model(neumf_model)
 
-    neumf_model.load_weights('./models/weights/neumf_weights.h5')
+    neumf_model.load_weights('./data/weights/neumf.h5')
+    
+    #Finding the jokes that the user has not rated 
+    unique_jokes_id = ratings['joke_id'].unique()
+    user_rated_jokes = ratings[ratings['user'] == user_id]['joke_id'].values
+    user_unrated_jokes = np.setdiff1d(unique_jokes_id, user_rated_jokes)
+    if user_unrated_jokes.size == 0:
+        return recommend_items_for_existing_user(user_id, num_recommendations=1)
+    
+    predictions = []
+    for joke_id in user_unrated_jokes:
+        pred = neumf_model.predict([np.array([user_id]), np.array([joke_id])])
+        predictions.append(pred)
+   
+    top_joke_idx = np.argmax(predictions)
+    top_joke = jokes.iloc[top_joke_idx]
+    return top_joke.values
 
 
 
 
-
-if __name__ == '__main__':
-    main()
